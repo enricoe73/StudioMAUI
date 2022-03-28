9a523aa700be6f6b8d3f67879166f415cbdd672c
Enrico Eusebi <Enrico.Eusebi@sicinformatica.it>
2022-03-28 11:51:16 +0200

    Add project files.

---
+++ StudioMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StudioMAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
